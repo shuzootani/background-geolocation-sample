8fb1a98996b4abc97752082b564a5bb71c1c41b2
shuzo <suzo.yolo@gmail.com>
2022-02-28 11:30:03 +0900

    make location app minimal

--- App.tsx
+++ App.tsx
@@ -1,125 +1,34 @@
-import React from 'react';
-import {
-  Linking,
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-import {
-  Button,
-  Icon,
-} from 'react-native-elements'
-
-import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import {HeaderBackButton} from '@react-navigation/elements';
-
-import HomeApp from "./src/home/HomeApp";
-import HelloWorldApp from "./src/hello-world/HelloWorldApp";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import AdvancedApp from "./src/advanced/AdvancedApp";
 
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-const NAVIGATION_KEY = "@transistorsoft:navigation";
-
 const MainStack = createNativeStackNavigator();
-
-// Custom back-button.  react-navigation does something messed up with default goBack() action.
-const screenOptions = ({ navigation }) => {
-
-  return {
-    headerShown: false
-  };
-}
-
-const App = () => {
-
-  const [isReady, setIsReady] = React.useState(false);
-  const [initialState, setInitialState] = React.useState();
-
-  React.useEffect(() => {
-    const restoreState = async () => {
-      try {
-        const initialUrl = await Linking.getInitialURL();
-
-        if (Platform.OS !== 'web' && initialUrl == null) {
-          // Only restore state if there's no deep link and we're not on web
-          const savedStateString = await AsyncStorage.getItem(NAVIGATION_KEY);
-          const state = savedStateString ? JSON.parse(savedStateString) : undefined;
-
-          if (state !== undefined) {
-            setInitialState(state);
-          }
-        }
-      } finally {
-        setIsReady(true);
-      }
-    };
-
-    if (!isReady) {
-      restoreState();
-    }
-  }, [isReady]);
-
-  if (!isReady) {
-    return null;
-  }
-
-  return (
-    <NavigationContainer
-      initialState={initialState}
-      onStateChange={(state) => {
-        AsyncStorage.setItem(NAVIGATION_KEY, JSON.stringify(state));
-      }}
-    >
-      <MainStack.Navigator
-        initialRouteName="HomeApp"
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: '#fedd1e'
-          },
-        }}>
-        <MainStack.Screen
-          name="HomeApp"
-          component={HomeApp}
-          options={{
-            headerShown: false
-          }}
-        />
-        <MainStack.Screen
-          name="AdvancedApp"
-          component={AdvancedApp}
-          options={screenOptions}
-        />
-        <MainStack.Screen
-          name="HelloWorldApp"
-          component={HelloWorldApp}
-          options={screenOptions}
-        />
-      </MainStack.Navigator>
-    </NavigationContainer>
-  );
+const screenOptions = () => {
+	return {
+		headerShown: false,
+	};
 };
 
-const styles = StyleSheet.create({
-  button: {
-    backgroundColor: '#fedd1e'
-  },
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    borderColor: '#000',
-    padding: 10,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-
+const App = () => {
+	return (
+		<NavigationContainer>
+			<MainStack.Navigator
+				initialRouteName="AdvancedApp"
+				screenOptions={{
+					headerStyle: {
+						backgroundColor: "#fedd1e",
+					},
+				}}
+			>
+				<MainStack.Screen
+					name="AdvancedApp"
+					component={AdvancedApp}
+					options={screenOptions}
+				/>
+			</MainStack.Navigator>
+		</NavigationContainer>
+	);
+};
 
 export default App;
--- src/advanced/HomeView.tsx
+++ src/advanced/HomeView.tsx
@@ -1,248 +1,70 @@
-/// Renders the main view of the AdvancedApp.useState
-///
-/// - Calls BackgroundGeolocation.ready.
-/// - Handles ON/OFF <Switch /> with BackgroundGeolocation.start / .stop()
-/// - [^] getCurrentPosition button.
-/// - [>] / [||] changePace button.
-///
-import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Switch,
-  TouchableHighlight,
-} from 'react-native';
-
-import {
-  Button,
-  Icon
-} from 'react-native-elements'
-
+import React from "react";
+import MapView, { Polyline } from "react-native-maps";
 import BackgroundGeolocation, {
-  Location,
-  State,
-  MotionActivityEvent
-} from "../react-native-background-geolocation";
+	Location,
+} from "react-native-background-geolocation";
 
-import SettingsService from './lib/SettingsService';
-import FABMenu from './FABMenu';
-import TSMapView from './TSMapView';
+const TSMapView = () => {
+	const [coordinates, setCoordinates] = React.useState<any[]>([]);
 
-import ENV from '../ENV';
-import {COLORS, SOUNDS} from './lib/config';
-import {registerTransistorAuthorizationListener} from '../lib/Authorization';
+	React.useEffect(() => {
+		BackgroundGeolocation.onLocation(onLocation, (error) => {
+			console.warn("[onLocation] ERROR: ", error);
+		});
 
-const HomeView = ({route, navigation}) => {
-  const {org, username} = route.params;
+		BackgroundGeolocation.ready({
+			debug: true,
+			desiredAccuracy: BackgroundGeolocation.DESIRED_ACCURACY_NAVIGATION,
+			maxDaysToPersist: 2,
+			stopOnTerminate: false,
+			startOnBoot: true,
+		}).then((state) => {
+			if (!state.enabled) {
+				BackgroundGeolocation.start();
+			}
+			BackgroundGeolocation.changePace(true);
+		});
 
-  const [enabled, setEnabled] = React.useState(false);
-  const [isMoving, setIsMoving] = React.useState(false);
-  const [location, setLocation] = React.useState<Location>(null);
-  const [odometer, setOdometer] = React.useState(0);
-  const [motionActivityEvent, setMotionActivityEvent] = React.useState<MotionActivityEvent>(null);
-  const [testClicks, setTestClicks] = React.useState(0);
-  const [clickBufferTimeout, setClickBufferTimeout] = React.useState<any>(0);
+		return () => {
+			setCoordinates([]);
+			BackgroundGeolocation.removeAllListeners();
+			BackgroundGeolocation.stop();
+		};
+	}, []);
 
-  // Handy Util class for managing app/plugin Settings.
-  const settingsService = SettingsService.getInstance();
+	/// Add a location Marker to map.
+	const onLocation = (location: Location) => {
+		if (location.sample || !location.coords) return;
+		setCoordinates((previous) => [
+			...previous,
+			{
+				latitude: location.coords.latitude,
+				longitude: location.coords.longitude,
+			},
+		]);
+	};
 
-  /// Init BackgroundGeolocation when view renders.
-  React.useEffect(() => {
-    // Register BackgroundGeolocation event-listeners.
+	return (
+		<MapView
+			region={{
+				latitude: 35.62832943167142,
+				longitude: 139.66781306093526,
+				latitudeDelta: 0.00922,
+				longitudeDelta: 0.00421,
+			}}
+			showsUserLocation={true}
+			style={{ flex: 1 }}
+		>
+			<Polyline
+				key="polyline"
+				coordinates={coordinates}
+				geodesic={true}
+				strokeColor="rgba(0,179,253, 0.6)"
+				strokeWidth={6}
+				zIndex={0}
+			/>
+		</MapView>
+	);
+};
 
-    // For printing odometer in bottom toolbar.
-    const locationSubscriber:any = BackgroundGeolocation.onLocation(setLocation, (error) => {
-      console.warn('[onLocation] ERROR: ', error);
-    });
-    // Auto-toggle [ play ] / [ pause ] button in bottom toolbar on motionchange events.
-    const motionChangeSubscriber:any = BackgroundGeolocation.onMotionChange((location) => {
-      setIsMoving(location.isMoving);
-    });
-    // For printing the motion-activity in bottom toolbar.
-    const activityChangeSubscriber:any = BackgroundGeolocation.onActivityChange(setMotionActivityEvent);
-
-    // Configure BackgroundGeolocation.ready().
-    initBackgroundGeolocation();
-
-    // Boilerplate authorization-listener for tracker.transistorsoft.com (nothing interesting)
-    registerTransistorAuthorizationListener(navigation);
-    return () => {
-      // When view is destroyed (or refreshed with dev live-reload),
-      // Remove BackgroundGeolocation event-listeners.
-      locationSubscriber.remove();
-      motionChangeSubscriber.remove();
-      activityChangeSubscriber.remove();
-    }
-  }, []);
-
-  /// Add a toggle <Switch> to top-right toolbar.
-  React.useLayoutEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <Switch onValueChange={onClickEnable} value={enabled} />
-      )
-    });
-  }, [enabled]);
-
-  /// Location effect-handler
-  React.useEffect(() => {
-    if (!location) return;
-    setOdometer(location.odometer);
-  }, [location]);
-
-  /// Handles test-mode clicks on bottom toolbar, ie: [activity:123km]
-  /// Reloads test-config.
-  React.useEffect(() => {
-    // Swallow the zero event.
-    if (testClicks === 0) return;
-
-    console.log('[TEST CLICK]:', testClicks);
-    settingsService.playSound('TEST_MODE_CLICK');
-    if (testClicks >= 10) {
-      // Hit it!
-      setTestClicks(0);
-      settingsService.playSound('TEST_MODE_SUCCESS');
-      settingsService.applyTestConfig();
-    } else if (testClicks <= 10) {
-      // Keep going...
-      if (clickBufferTimeout > 0) {
-        clearTimeout(clickBufferTimeout);
-      }
-      setClickBufferTimeout(setTimeout(() => {
-        setTestClicks(0);
-      }, 2000));
-    }
-  }, [testClicks]);
-
-  /// Configure BackgroundGeolocation.ready
-  const initBackgroundGeolocation = async () => {
-    // Get an authorization token from transistorsoft demo server.
-    const token = await BackgroundGeolocation.findOrCreateTransistorAuthorizationToken(
-      org,
-      username,
-      ENV.TRACKER_HOST
-    );
-
-    // Ready the SDK and fetch the current state.
-    const state:State = await BackgroundGeolocation.ready({
-      // Debug
-      reset: false,
-      debug: true,
-      logLevel: BackgroundGeolocation.LOG_LEVEL_VERBOSE,
-      transistorAuthorizationToken: token,
-      // Geolocation
-      desiredAccuracy: BackgroundGeolocation.DESIRED_ACCURACY_NAVIGATION,
-      distanceFilter: 10,
-      stopTimeout: 5,
-      // Permissions
-      locationAuthorizationRequest: 'Always',
-      backgroundPermissionRationale: {
-        title: "Allow {applicationName} to access this device's location even when closed or not in use.",
-        message: "This app collects location data to enable recording your trips to work and calculate distance-travelled.",
-        positiveAction: 'Change to "{backgroundPermissionOptionLabel}"',
-        negativeAction: 'Cancel'
-      },
-      // HTTP & Persistence
-      autoSync: true,
-      maxDaysToPersist: 14,
-      // Application
-      stopOnTerminate: false,
-      startOnBoot: true,
-      enableHeadless: true
-    });
-
-    setOdometer(state.odometer);
-    setEnabled(state.enabled);
-    setIsMoving(state.isMoving||false);  // <-- TODO re-define @prop isMoving? as REQUIRED in State
-  };
-
-  /// <Switch> handler to toggle the plugin on/off.
-  const onClickEnable = (value:boolean) => {
-    setEnabled(value);
-    if (value) {
-      BackgroundGeolocation.start();
-    } else {
-      BackgroundGeolocation.stop();
-      // Toggle the [ > ] / [ || ] button in bottom-toolbar back to [ > ]
-      setIsMoving(false);
-    }
-  }
-
-  /// getCurrentPosition handler
-  const onClickGetCurrentPosition = () => {
-    settingsService.playSound('BUTTON_CLICK');
-
-    BackgroundGeolocation.getCurrentPosition({
-      persist: true,
-      samples: 1,
-      timeout: 30,
-      extras: {
-        getCurrentPosition: true
-      }
-    }).then((location:Location) => {
-      console.log('[getCurrentPosition] success: ', location);
-    }).catch((error:LocationError) => {
-      console.warn('[getCurrentPosition] error: ', error);
-    });
-  }
-
-  /// changePace handler.
-  const onClickChangePace = () => {
-    BackgroundGeolocation.changePace(!isMoving);
-    setIsMoving(!isMoving);
-  }
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <TSMapView style={styles.map} navigation={navigation} />
-      <View style={{backgroundColor: COLORS.gold, height: 56, flexDirection: 'row'}}>
-        <View style={{justifyContent:'center'}}>
-          <Button
-            type="clear"
-            onPress={onClickGetCurrentPosition}
-            containerStyle={{width: 60}}
-            icon={<Icon name='navigate-sharp' type='ionicon' /> }
-          />
-        </View>
-        <View style={{flex: 1, justifyContent: 'center'}}>
-          <TouchableHighlight onPress={() => setTestClicks(testClicks + 1)} underlayColor="transparent">
-            <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-              <Text style={styles.statusBar}>{(motionActivityEvent) ? motionActivityEvent.activity : 'unknown'}</Text>
-              <Text>&nbsp;•&nbsp;</Text>
-              <Text style={styles.statusBar}>{(odometer/1000).toFixed(1)}km</Text>
-            </View>
-          </TouchableHighlight>
-        </View>
-        <View style={{justifyContent: 'center', padding: 5}}>
-          <Button
-            containerStyle={{width: 60}}
-            buttonStyle={{backgroundColor: (isMoving) ? COLORS.red : COLORS.green}}
-            onPress={onClickChangePace}
-            icon={<Icon name={(isMoving) ? 'pause-sharp' : 'play-sharp'} type='ionicon' color={COLORS.white}/> }
-          />
-        </View>
-      </View>
-
-      <FABMenu navigation={navigation} onResetOdometer={(location:Location) => setOdometer(location.odometer)}/>
-
-    </SafeAreaView>
-  );
-}
-
-export default HomeView;
-
-var styles = StyleSheet.create({
-  container: {
-    backgroundColor: COLORS.gold,
-    flexDirection: 'column',
-    flex: 1
-  },
-  map: {
-    flex: 1,
-  },
-  statusBar: {
-    fontSize: 16
-  }
-});
+export default TSMapView;
